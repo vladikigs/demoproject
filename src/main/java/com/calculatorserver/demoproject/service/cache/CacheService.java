188b57d7cb393cbd284e165323620453600e0c50
vlad <vladikigs@mail.ru>
2021-01-29 21:27:28 +0400

    Multithreading cache

--- src/main/java/com/calculatorserver/demoproject/service/cache/CacheService.java
+++ src/main/java/com/calculatorserver/demoproject/service/cache/CacheService.java
@@ -1,34 +1,37 @@
 package com.calculatorserver.demoproject.service.cache;
 
 import com.calculatorserver.demoproject.dto.CalculateRequest;
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
+@ThreadSafe
 public class CacheService {
 
 
     private Integer cacheSize;
-    private final LinkedHashMap<CalculateRequest, String> unsafeCacheValues = new LinkedHashMap<>();
-    private final Map<CalculateRequest, String> cacheValues = Collections.synchronizedMap(unsafeCacheValues);
+    @GuardedBy("this")
+    private final LinkedHashMap<CalculateRequest, String> cacheValues = new LinkedHashMap<>();
+
 
     @Value("${cache.sizeCache}")
     public void setSizeCache(Integer sizeCache) {
         this.cacheSize = sizeCache;
     }
 
-    public void addElementInCache(CalculateRequest calculateRequest, String calculate) {
-        Iterator<Map.Entry<CalculateRequest, String>> iterator = cacheValues.entrySet().iterator();
-        if (cacheValues.size() >= cacheSize) {
-            cacheValues.remove(iterator.next().getKey());
-        }
-        cacheValues.put(calculateRequest, calculate);
-
+    public synchronized void addElementInCache(CalculateRequest calculateRequest, String calculate) {
+            Iterator<Map.Entry<CalculateRequest, String>> iterator = cacheValues.entrySet().iterator();
+            if (cacheValues.size() >= cacheSize) {
+                cacheValues.remove(iterator.next().getKey());
+            }
+            cacheValues.put(calculateRequest, calculate);
     }
 
-    public String findElementInCache(CalculateRequest calculateRequest) {
+    public synchronized String findElementInCache(CalculateRequest calculateRequest) {
         return cacheValues.get(calculateRequest);
     }
 
